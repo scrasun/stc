68b0910916d939df70faa6d7ee8d38257130b915
samuel <samuel@sams-laptop>
2021-03-14 14:50:18 +0000

    initial project

---
+++ src/test/java/com/scheshire/stc/StcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheshire.stc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
